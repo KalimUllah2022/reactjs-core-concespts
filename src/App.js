6ae606f41074b64085b930b50c99ee17d56c0dbe
KalimUllah2022 <villageprogrammer2022@gmail.com>
2022-03-22 01:28:25 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
+import logo from "./logo.svg";
+import "./App.css";
+const kali = {
+  Name: 'Kalim Ullah', Profession: 'Software Engineer'
+}
+const number = 55555;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Kalim Ullah</h1>
+      <Person></Person>
+      <h5>New Components. YAY</h5>
+      <Friend></Friend>
+      <Kali></Kali>
+      <p>Name: { kali.Name}</p>
+      <i>Name: {kali.Profession}</i>
+      <br />
+      <span>Total: {11111 + number}</span>
     </div>
   );
+}
+function Person() {
+  return (
+    <div className='person'>
+      <h1>Sakib Al Hasan</h1>
+      <p>Profession: Cricket</p>
+    </div>
+  );
+}
+
+function Friend() {
+  return (
+    <div class='container'>
+      <a href="https://youtube.com">Click Hire!</a>
+      <h3>Name: Ajay Devgun</h3>
+      <p>Job: Maramari</p>
+    </div>
+  )
+}
+
+function Kali() { 
+  return (
+    <div class="myDiv">
+      <a href="https://youtube.com">Click Hire!</a>
+      <p>Lorem Ipsum</p>
+    </div>
+  )
 }
 
 export default App;
